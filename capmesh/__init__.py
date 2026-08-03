0eceb3d8cfec7921bc0e1ea535869c4a55e3ffd4
Theinatorinator <Logan.mamanakis@gmail.com>
2026-08-03 14:50:23 -0600

    feat(util): add reflection based version grabbing

--- capmesh/__init__.py
+++ capmesh/__init__.py
@@ -3,3 +3,13 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 __app_name__ = "CAPmesh"
+
+import importlib.metadata
+
+try:
+  __version__: str = importlib.metadata.version(distribution_name=__app_name__)
+except (importlib.metadata.PackageNotFoundError, AttributeError, IndexError):
+  __version__ = "0.0.0-unknown"
+
+# Explicitly define what is exposed when importing from the package root
+__all__: list[str] = ["__version__"]
